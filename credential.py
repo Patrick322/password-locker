8a248b9b6908fdfd7e3d5fb41afce6a39533e6f2
Patrick595 <patrickbuong@gmail.com>
2020-01-26 15:59:07 +0300

    added __init__method that will allows us to create new instances

--- credential.py
+++ credential.py
@@ -1,4 +1,13 @@
 class credential:
     """
     Class that generates new instances of users
+    """
+    credentials_list  = []
+
+    def __init__(self,credential_name,user_name,password,email):
+        self.credential_name = credential_name
+        self.user_name = user_name
+        self.password = password
+        self.email = email
+
     
