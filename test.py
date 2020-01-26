b45b588f6084f125cbb078ccb5fea978c9749f7a
Patrick595 <patrickbuong@gmail.com>
2020-01-26 14:12:03 +0300

    added a function that will now test for the credential for my personal account

--- test.py
+++ test.py
@@ -5,4 +5,7 @@ from credential import credential
 class TestCredentials(unittest.TestCase):
     def SetUp(self)
 
-    self.new_credentials = Credential("Patrick","Buong","")
+    self.new_credentials = Credential("Patrick","Buong","37897750","patrickbuong@gmail.com") # create account object
+
+
+    def test_init(self):
