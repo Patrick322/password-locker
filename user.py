7b7d80edbb52de8abdb8b26e4ba63c6d4c5e9c3d
Patrick595 <patrickbuong@gmail.com>
2020-01-26 16:41:17 +0300

    added a function that will return true if the input details are valid

--- user.py
+++ user.py
@@ -13,3 +13,30 @@ class user:
         self.password = password
 
     user_list = []
+def delete_user(self):
+
+
+
+        user.user_list.append(self)
+    def delete_user(self):
+
+
+
+    user.user_list.append(self)
+@classmethod
+def find_by_user_name(cls,user_name):
+
+
+    for user in cls.user_list:
+        if user.user_name == user_name:
+            return user_name
+@classmethod
+def user_exist(cls,user_name):
+
+        for user in cls.user_list:
+            if user.user_name == user_name:
+                return True
+
+        return False
+    @classmethod
+    def display_user_name(cls)
