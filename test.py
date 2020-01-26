e627ac00a708d3cc96fd12934647cfb99f56f551
Patrick595 <patrickbuong@gmail.com>
2020-01-26 14:08:51 +0300

    added a function for personal test

--- test.py
+++ test.py
@@ -1,2 +1,8 @@
 import unittest
-from credentials
+from credential import credential
+
+
+class TestCredentials(unittest.TestCase):
+    def SetUp(self)
+
+    self.new_credentials = Credential("Patrick","Buong","")
