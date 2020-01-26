b4eaab0dbf0445aa8950d0b2006b9a8f9050596f
Patrick595 <patrickbuong@gmail.com>
2020-01-26 16:28:27 +0300

    added method that will create instances for the user names and password

---
+++ user.py
@@ -0,0 +1,15 @@
+import pyperclip
+class user:
+
+    user_list = []
+
+    def __init__(self,first_name,last_name,user_name,password):
+
+
+
+        self.first_name = first_name
+        self.last_name = last_name
+        self.user_name = user_name
+        self.password = password
+
+    user_list = []
