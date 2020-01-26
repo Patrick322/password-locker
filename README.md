34cbd43307ef63c63d265b1ccda65f75550eb6dd
Patrick595 <patrickbuong@gmail.com>
2020-01-26 17:43:27 +0300

    modified the README.md

---
+++ README.md
@@ -0,0 +1,52 @@
+## Project Name
+Password-locker
+
+##  Author
+
+Buong Patrick
+
+## Description
+
+This is a terminal working project where one us to store his/her account and password,he/she can also create an account and also generate a password.
+Password Locker is a python application that helps users generate and store passwords for their accounts such as facebook, twitter, gmail.
+
+## Setup Instructions
+
+* clone this repository
+
+* have python3.6 on your device
+
+* Run application in your terminal using ./run.py
+
+* Test application using $ python3.6 user_test.py
+
+* Once you run the App follow the instructions on the terminal
+
+
+## Behavior Driven Development (BDD)
+
+* cu - Creates a new account
+
+* lg - logs in
+
+* ex - Exit the ap The user will be met with the following commands while signing in:
+
+* ap - Add password
+
+* cp - Copy password to clipboard
+
+* lo - Log out
+
+
+## Technology Used
+
+Python3.6
+
+## Contacts Info
+
+    TEl:+254797239875
+    Email:patrickbuong@gmail.com
+
+## Licence
+
+licenced under the[MIT licence](LICENCED)
---
+++ user_test.py
@@ -0,0 +1,81 @@
+import unittest
+import pyperclip
+from user import User
+
+class TestUser(unittest.TestCase):
+    
+     def setUp(self):
+         
+         
+          self.new_user = User("Patrick","Buong","Patoh Patrick","pachecoh")
+          
+          
+          
+     def test_init(self):
+          
+        self.assertEqual(self.new_user.first_name,"Patrick")
+        self.assertEqual(self.new_user.last_name,"Buong")
+        self.assertEqual(self.new_user.user_name,"Patoh Patrick")
+        self.assertEqual(self.new_user.password,"pachecoh")
+     
+               
+         
+     def test_save_user(self):
+       
+        self.new_user.save_user() 
+        self.assertEqual(len(User.user_list),1)
+        
+     def test_save_multiple_user(self):
+       
+        self.new_user.save_user()
+        test_user = User("Facebook","Patrick","Patoh Patrick","Pachecoh") 
+        test_user.save_user()
+        self.assertEqual(len(User.user_list),2)
+    
+     def tearDown(self):
+         
+         User.user_list = []
+
+     def test_delete_user(self):
+        
+         self.new_user.save_user()
+         test_user = User("Facebook","Patrick","Patoh Patrick","Pachecoh") 
+         test_user.save_user()
+
+         self.new_user.delete_user()
+         self.assertEqual(len(User.user_list),1)
+     def test_find_user_by_username(self):
+         
+
+         self.new_user.save_user()
+         test_user = User("Facebook","Patrick,"Patoh Patrick","Pachecoh")
+         test_user.save_user()
+
+         found_user = User.find_by_user_name("Patoh Patrick")
+
+         self.assertEqual(found_user.user_name,test_user.user_name)
+     def test_user_exists(self):
+         
+
+         self.new_user.save_user()
+         test_user = User("Facebook","Patrick","Patoh Patrick","pachecoh") 
+         test_user.save_user()
+
+         user_exists = User.user_exist("Patoh Patrick")
+
+         self.assertTrue(user_exists)
+     def test_display_all_users(self):
+       
+
+         self.assertEqual(User.display_user(),User.user_list)
+   
+     def test_copy_password(self):
+         
+
+         self.new_user.save_user()
+         User.copy_password("")
+
+         self.assertEqual(self.new_user.password,pyperclip.paste())
+
+if __name__ ==  '__main__':
+ unittest.main()
